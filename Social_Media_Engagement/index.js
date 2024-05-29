fa2ef36054ef06ecac38a111cd6a322ddd73b74e
Sai Vineela Madamanchi <133285349+vineela1511@users.noreply.github.com>
2024-05-29 16:24:45 -0500

    Update index.js

--- Social_Media_Engagement/index.js
+++ Social_Media_Engagement/index.js
@@ -1,5 +1,5 @@
 
-const express = require('/Users/smadama/node_modules/express');
+const express = require('/Users/user_name/node_modules/express');
 const app = express();
 app.listen(3000, ()=> console.log('listening at 3000'))
 app.use(express.static('public'))
